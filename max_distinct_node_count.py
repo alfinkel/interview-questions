7621c6aa091d124901ade1a0951dd27543ebecfb
Al Finkelstein <alfinkel@us.ibm.com>
2017-05-07 14:05:08 -0500

    Create max_distinct_node_count.py

---
+++ max_distinct_node_count.py
@@ -0,0 +1,63 @@
+class Tree(object):
+    x = 0
+    l = None
+    r = None
+
+def solution(T):
+    """
+    Return the count for the maximum number of distinct
+    nodes in a branch for the tree T.  Given the definition
+    of tree as above.
+    """
+    max_distinct_values = 0
+    for path in get_paths_better(T):
+        distinct_values = len(set(path))
+        if distinct_values > max_distinct_values:
+            max_distinct_values = distinct_values
+    return max_distinct_values
+    
+def get_paths(node):
+    """
+    Hacked up way to get a list of branches recursively.
+    """
+    if node is None:
+        return []
+    if node.l is None and node.r is None:
+        return [node.x]
+
+    paths = []
+    for path in get_paths(node.l) + get_paths(node.r):
+        if not isinstance(path, list):
+            path = [path]
+        paths.append([node.x] + path)
+
+    return paths
+
+def get_paths_better(root):
+    """
+    Instead of recursing through the tree, use stacks to
+    track the traversal of the tree and to generate the
+    list of branches from the tree (root).
+    """
+    # Assign the root to the top of the stack
+    tree_nodes = [root]
+    paths = [[root.x]]
+    branches = []
+    while (len(tree_nodes) > 0):
+        # Pop the top of the stack
+        node = tree_nodes.pop()
+        path = paths.pop()
+        # If you reached a leaf then add the path (branch)
+        # to the branches list, otherwise add nodes to the
+        # stack as appropriate
+        if node.l is None and node.r is None:
+            branches.append(path)
+        else:
+            if node.r is not None:
+                tree_nodes.append(node.r)
+                paths.append(path + [node.r.x])
+            if node.l is not None:
+                tree_nodes.append(node.l)
+                paths.append(path + [node.l.x])
+
+    return branches
